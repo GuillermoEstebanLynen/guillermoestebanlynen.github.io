ddab5fbfdf16681b82aa7d9e42d55954f3abca0a
Guilllermo Lynen <lynenguillermo@gmail.com>
2024-10-06 16:33:34 -0300

    Primer Version Navegacion Movil

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 // Selecciona el botón de toggle y el menú
+const containerMenu = document.getElementById('containerHeader');
 const menuToggle = document.getElementById('menu-toggle');
 const navMenu = document.getElementById('nav-menu');
 
 // Agrega el evento de click para mostrar/ocultar el menú
 menuToggle.addEventListener('click', () => {
+    containerMenu.classList.toggle('expandMobile');
     navMenu.classList.toggle('show');
 });
 
-
 function scrollToSection( seccion ) {
+    containerMenu.classList.toggle('expandMobile');
     navMenu.classList.toggle('show');
     const section = document.getElementById(seccion);
     section.scrollIntoView({ behavior: "smooth" });
