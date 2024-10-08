bed7855dd4728234ddb68731f2b2fadafc6da766
Guilllermo Lynen <lynenguillermo@gmail.com>
2024-10-08 11:13:28 -0300

    Mejora navegacion Link

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 // Selecciona el botón de toggle y el menú
 const containerMenu = document.getElementById('containerHeader');
-const menuToggle = document.getElementById('menu-toggle');
-const navMenu = document.getElementById('nav-menu');
+const menuToggle = document.getElementById('menuToggle');
+const navMenu = document.getElementById('navMenu');
+
+let linkActive = 'infoLink';
 
 // Agrega el evento de click para mostrar/ocultar el menú
 menuToggle.addEventListener('click', () => {
@@ -10,8 +12,17 @@ menuToggle.addEventListener('click', () => {
 });
 
 function scrollToSection( seccion ) {
+    // Desactivo link activo anterior
+    document.getElementById(linkActive).classList.toggle('active');
+
+    // Activo nuevo link
+    linkActive = seccion + 'Link';
+    document.getElementById( linkActive ).classList.toggle('active');
+
+    // Cierro menu movil
     containerMenu.classList.toggle('expandMobile');
     navMenu.classList.toggle('show');
-    const section = document.getElementById(seccion);
-    section.scrollIntoView({ behavior: "smooth" });
+
+    // Hago scrool a la seccion correspondiente
+    document.getElementById(seccion).scrollIntoView({ behavior: "smooth" });
 }
